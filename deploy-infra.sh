202f10bba26d70ced173b6dac09d2c0ccef3b62b
nuel07 <yehanganep@gmail.com>
2024-03-12 15:03:54 +0300

    Creating CodePipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -43,3 +43,10 @@ aws cloudformation deploy \
     GitHubBranch=$GH_BRANCH \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET \
+
+    # If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --query "Exports[?Name=='InstanceEndpoint'].Value" 
+fi
